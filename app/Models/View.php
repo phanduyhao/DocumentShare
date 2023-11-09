dc90971de95e7d0bdcdca7b7e755a0f6e8b3f960
phanduyhao <haomrvuii@gmail.com>
2023-11-09 14:17:38 +0700

    update

--- app/Models/View.php
+++ app/Models/View.php
@@ -8,11 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class View extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
     protected $table = 'views';
 
-=======
->>>>>>> f2c7389ac3cb13c8ba21f3c2ee2c19c43e10fea7
     public function Document()
     {
         return $this->belongsTo( Document::class, 'document_id','id');
